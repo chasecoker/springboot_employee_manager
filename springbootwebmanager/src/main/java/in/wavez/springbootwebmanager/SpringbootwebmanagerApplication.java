50fd27e9f5a9d381c1846ec7eeae1524af70496a
chasecoker <chasewavez@gmail.com>
2022-10-29 15:52:31 -0500

    the first commit

---
+++ springbootwebmanager/src/main/java/in/wavez/springbootwebmanager/SpringbootwebmanagerApplication.java
@@ -0,0 +1,13 @@
+package in.wavez.springbootwebmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebmanagerApplication.class, args);
+	}
+
+}
---
+++ springbootwebmanager/src/test/java/in/wavez/springbootwebmanager/SpringbootwebmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.wavez.springbootwebmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
